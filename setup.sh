34e3e4acd05686f5d274ae85b467cc2eae72e853
pman <pman-$-432894328923@gmail.com>
2024-08-21 16:59:02 +0200

    ...

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 ./build.sh setup.jsonnet
+
 kubectl create ns actions-runner-system
+
 kubectl apply --server-side -f manifests/setup
 kubectl wait --for condition=Established --all CustomResourceDefinition -n monitoring
+
 kubectl apply -f manifests/
+
+kubectl -n monitoring port-forward svc/prometheus-k8s 9090:9090 &
