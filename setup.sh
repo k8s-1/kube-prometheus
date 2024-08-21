451b09a545fa0ab44d6b6b872fb7035968606a93
pman <pman-$-432894328923@gmail.com>
2024-08-21 16:57:09 +0200

    ...

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./build.sh setup.jsonnet
+kubectl create ns actions-runner-system
 kubectl apply --server-side -f manifests/setup
 kubectl wait --for condition=Established --all CustomResourceDefinition -n monitoring
 kubectl apply -f manifests/
