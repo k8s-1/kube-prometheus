938c398a1c268b67d39c2bfd571ffd9f4911a0ce
pman <pman-$-432894328923@gmail.com>
2024-08-21 17:31:56 +0200

    ...

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ kubectl wait --for condition=Established --all CustomResourceDefinition -n monit
 
 kubectl apply -f manifests/
 
-kubectl wait --for=condition=ready pod/prometheus-k8s -n monitoring
-kubectl -n monitoring port-forward svc/prometheus-k8s 9090:9090 &
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=prometheus -n monitoring
+kubectl -n monitoring port-forward svc/prometheus-k8s 9090:9090 > /dev/null 2>&1 &
