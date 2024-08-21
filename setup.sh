810c4bfc25bbbb1ac086640bb0f21256860ae145
pman <pman-$-432894328923@gmail.com>
2024-08-21 17:14:19 +0200

    chore: update file(s): setup.sh

--- setup.sh
+++ setup.sh
@@ -8,4 +8,5 @@ kubectl wait --for condition=Established --all CustomResourceDefinition -n monit
 
 kubectl apply -f manifests/
 
+kubectl wait -n kube-system --for=jsonpath='{.status.phase}'=Running pod/busybox1
 kubectl -n monitoring port-forward svc/prometheus-k8s 9090:9090 &
