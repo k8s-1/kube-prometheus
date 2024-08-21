34e366a44a07aa1339dfd6c8b0ec2ca3ca05f8b6
pman <pman-$-432894328923@gmail.com>
2024-08-21 17:50:07 +0200

    ...

--- setup.sh
+++ setup.sh
@@ -11,6 +11,6 @@ kubectl apply -f manifests/
 kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=prometheus -n monitoring --timeout 300s
 kubectl -n monitoring port-forward svc/prometheus-k8s 9090:9090 > /dev/null 2>&1 &
 
-echo
-echo "Metrics are being monitored:"
-echo "http://localhost:9090"
+printf "\nMetrics are being monitored:"
+url="http://localhost:9090/targets"
+echo -e "\e[4m$url\e[0m"
